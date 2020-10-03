a03d331ae23ce7d6d7af28a1950a653d574c7a62
click-to-generate-name <k4s.txt@gmail.com>
2020-10-03 21:22:11 +0200

    Extend to multiple values

--- question1.asm
+++ question1.asm
@@ -10,16 +10,37 @@ main:
 	syscall # Print prompt
 	
 	li $v0, 8 # Load read_string opcode into v0
-	la $a0, bfr # Load empty byte space into a0
-	li $a1, 4 # Load size of byte space to allot into a1
+	li $a1, 10 # Load size of byte space to allot into a1
 	
+	# Reading and printing values is not in a loop because idk how
+	# to change store address based on counter value
+	
+	# Value 1
+	la $a0, bfr1 # Load empty byte space into a0
 	syscall # Read input
-	la $t0, bfr # Store input in temp register
+	la $t1, bfr1 # Store input in temp register
 	
-	li $v0, 4 # Load print_string opcode into $vo
-	li $v0, 4 # Print entered value
+	# Value 2
+	la $a0, bfr2
+	syscall
+	la $t2, bfr2
+	
+	li $v0, 4 # Load print_string opcode into $v0
+	
+	# Value 1
+	move $a0, $t1 # Load value into $a0
+	syscall # Print value
+	
+	# Value 2
+	move $a0, $t2
 	syscall
 
 .data
-	msg1: .asciiz "Enter a series of 5 formulae:\n"
-	bfr: .space 10
+	msg1: .asciiz "Enter a series of formulae:\n"
+	
+	bfr1: .space 10
+	bfr2: .space 10
+	bfr3: .space 10
+	bfr4: .space 10
+	bfr5: .space 10
+
