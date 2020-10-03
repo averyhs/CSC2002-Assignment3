1134394390a729410551397f621f2c0f0ed837c1
click-to-generate-name <k4s.txt@gmail.com>
2020-10-03 21:43:59 +0200

    Clean up comments

--- question1.asm
+++ question1.asm
@@ -5,61 +5,68 @@
 
 .text
 main:
-	li $v0, 4 # Load print_string opcode into v0
-	la $a0, msg1 # Load user prompt string into a0
-	syscall # Print prompt
+	# Nothing is looped because idk how to change
+	# addresses and labels based on value of counter
 	
-	li $v0, 8 # Load read_string opcode into v0
-	li $a1, 10 # Load size of byte space to allot into a1
+	# Print message to prompt user for values
+	# ---------------------------------------
+	li  $v0, 4     # Load print_string opcode into v0
+	la  $a0, msg1  # Load user prompt string into a0
+	syscall        # Print prompt
 	
-	# Reading and printing values is not in a loop because idk how
-	# to change store address based on counter value
+	# Read in values
+	# --------------
+	li  $v0, 8     # Load read_string opcode into v0
+	li  $a1, 10    # Load size of byte space to allot into a1
 	
-	# Value 1
-	la $a0, bfr1 # Load empty byte space into a0
-	syscall # Read input
+	# Value 1:
+	la  $a0, bfr1  # Load empty byte space into a0
+	syscall        # Read input
 	#la $t1, bfr1 # Store input in temp register
 	
-	# Value 2
-	la $a0, bfr2
+	# Value 2:
+	la  $a0, bfr2
+	syscall
+	# Value 3:
+	la  $a0, bfr3
+	syscall
+	# Value 4:
+	la  $a0, bfr4
+	syscall
+	# Value 5:
+	la  $a0, bfr5
 	syscall
 	
-	# Value 3
-	la $a0, bfr3
-	syscall
+	# Print message to display entered values
+	# ---------------------------------------
+	li  $v0, 4     # Load print_string opcode into $v0
+	la  $a0, msg2  # Load print back message string into a0
+	syscall        # Print
 	
-	# Value 4
-	la $a0, bfr4
-	syscall
-	
-	# Value 5
-	la $a0, bfr5
-	syscall
-	
-	li $v0, 4 # Load print_string opcode into $v0
-	la $a0, msg2 # Load print back message string into a0
-	syscall # Print
-	
-	# Value 1
+	# Print values
+	# ------------
+	# Value 1:
 	#move $a0, $t1 # Load value into $a0
-	la $a0, bfr1 # Load value into $a0
-	syscall # Print value
+	la  $a0, bfr1  # Load value into $a0
+	syscall        # Print value
 	
-	# Value 2
-	la $a0, bfr2
+	# Value 2:
+	la  $a0, bfr2
+	syscall
+	# Value 3:
+	la  $a0, bfr3
+	syscall
+	# Value 4:
+	la  $a0, bfr4
+	syscall
+	# Value 5:
+	la  $a0, bfr5
 	syscall
 	
-	# Value 3
-	la $a0, bfr3
-	syscall
-	
-	# Value 4
-	la $a0, bfr4
-	syscall
-	
-	# Value 5
-	la $a0, bfr5
-	syscall
+	# Exit
+	# ----
+	li  $v0, 10   # Load exit opcode into v0
+	syscall       # Exit
 
 .data
 	msg1: .asciiz "Enter a series of 5 formulae:\n"
